715a9305ae83cd8c6d3eaf07bfc9fe1018c413fb
yvettemuki <373628977@qq.com>
2023-12-07 10:10:00 -0500

    feat: resolution scale

--- public/app.js
+++ public/app.js
@@ -596,33 +596,34 @@ let sendBandwidthLevel = 5;
 
 function dynamicUpdateResolution(bandwidth) {
     //estimated R = 0.5; 0.6Mbps = 200*200*30
+    const ratio = 360.0 / 480.0;
     if (remoteVideo) {
         const activeUsersCount = remoteVideo.childElementCount;
         const bandwidth_partition = bandwidth / activeUsersCount;
 
         if (bandwidth_partition >= 3750000 && sendBandwidthLevel != 5) {
             sendBandwidthLevel = 5;
-            changeVideoResolution(500, 500);
+            changeVideoResolution(500, 500 * ratio);
         }
         else if (bandwidth_partition < 3750000 && bandwidth_partition >= 2400000 && sendBandwidthLevel != 4) {
             sendBandwidthLevel = 4;
-            changeVideoResolution(400, 400);
+            changeVideoResolution(400, 400 * ratio);
         }
         else if (bandwidth_partition < 2400000 && bandwidth_partition >= 1350000 && sendBandwidthLevel != 3) {
             sendBandwidthLevel = 3;
-            changeVideoResolution(300, 300);
+            changeVideoResolution(300, 300 * ratio);
         }
         else if (bandwidth_partition < 1350000 && bandwidth_partition >= 600000 && sendBandwidthLevel != 2) {
             sendBandwidthLevel = 2;
-            changeVideoResolution(200, 200);
+            changeVideoResolution(200, 200 * ratio);
         }
         else if (bandwidth_partition < 600000 && bandwidth_partition >= 150000 && sendBandwidthLevel != 1) {
             sendBandwidthLevel = 1;
-            changeVideoResolution(100, 100);
+            changeVideoResolution(100, 100 * ratio);
         }
         else if (bandwidth_partition < 150000 && sendBandwidthLevel != 0) {
             sendBandwidthLevel = 0;
-            changeVideoResolution(50, 50);
+            changeVideoResolution(50, 50 * ratio);
         }
     }
     else {
